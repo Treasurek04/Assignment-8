6794a89df80118429c156d66777f8bcc6048bebb
Treasurek04 <treasurekeys2004@gmail.com>
2024-12-04 00:13:45 -0600

    Import Asset, resize items, and created Target script

---
+++ Prototype 5/Assets/Scripts/Target.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private RigidBody targetRb;
+    private float minSpeed = 12;
+    private float maxSpeed = 16;
+    private float maxTorgue = 10;
+    private float xRange = 4;
+    private float ySpawnPos = -6;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRb = GetComponent<RigidBody>();
+
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+
+        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+
+        transform.position = RandomSpawnPos();
+
+
+    }
+
+    private Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
+    }
+    
+    private float RandomTorque()
+    {
+        return Random.Range(- maxTorque, maxTorgue);
+    }
+
+    private Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
