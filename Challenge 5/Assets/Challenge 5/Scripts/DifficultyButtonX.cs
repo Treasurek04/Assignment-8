47c34d8b5e2e1175056e3702cbad06216752c4ed
Treasurek04 <treasurekeys2004@gmail.com>
2024-12-04 04:22:55 -0600

    Challenge 5 is done
    The text is fixed, restart button works, timer and score works correctly, and everything is working smoothly

---
+++ Challenge 5/Assets/Challenge 5/Scripts/DifficultyButtonX.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyButtonX : MonoBehaviour
+{
+    private Button button;
+    private GameManagerX gameManagerX;
+    public int difficulty; // Value representing difficulty (e.g., 1 for Easy, 2 for Medium, 3 for Hard)
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManagerX = GameObject.Find("Game Manager").GetComponent<GameManagerX>();
+        button = GetComponent<Button>();
+        button.onClick.AddListener(SetDifficulty);
+    }
+
+    // Sets the difficulty and starts the game
+    void SetDifficulty()
+    {
+        Debug.Log(button.gameObject.name + " was clicked");
+        gameManagerX.StartGame(difficulty); // Pass the difficulty value to StartGame
+    }
+}
